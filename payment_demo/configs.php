d86ec4d6076c4308783eb556201bb83228713b2e
richardtomiwa <101923650+richardtomiwa@users.noreply.github.com>
2025-05-05 13:12:51 +0100

    Update configs.php

--- payment_demo/configs.php
+++ payment_demo/configs.php
@@ -1,4 +1,4 @@
 <?php
-$SecretKey = "sk_live_411a1a69efb728ffaec22a145f69df9f2ff0ce8a"; // Add your secret key here. Remember to change this to your live secret key in production
-$PublicKey = "pk_live_affe73d0b3e1827a39b2fb0f4a9d219300a0ec91"; // Add your public key here. Remember to change this to your live public key in production
+$SecretKey = "" // Add your secret key here. Remember to change this to your live secret key in production
+$PublicKey = ""; // Add your public key here. Remember to change this to your live public key in production
 ?>
